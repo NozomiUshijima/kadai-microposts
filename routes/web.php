1490a8a1a76d02a75d209383b8be7ca74a0bd1ba
nozomi.ushijima <yrznet217113@yahoo.co.jp>
2020-08-05 08:37:14 +0000

    user pages error collection

--- routes/web.php
+++ routes/web.php
@@ -25,5 +25,5 @@ Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
 Route::group(['middleware' => ['auth']], function () {
-    Route::resource('users', 'UserController', ['only' => ['index', 'show']]);
+    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
 });
